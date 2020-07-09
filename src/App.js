c675e7062c084455ba3ace75de0ff48d45b9b120
hasansattar <hasansattar650@gmail.com>
2020-07-09 12:46:02 +0500

    updated files all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styles from './App.module.css';
+
+import Cards from './components/Cards/Cards.js'
+import Chart from './components/Chart/Chart.js'
+import CountryPicker from './components/CountryPicker/CountryPicker.js'
+
+import coronaimage from './Image/covid19.png'
+
+import { fetchData } from './api/index.js'
+
 
 function App() {
+
+  const [state, setstate] = useState([{}])
+  const country = useState("")
+
+
+
+
+
+  useEffect(() => {
+    async function getRepo() {
+      const dataAPI = await fetchData();
+
+      setstate(dataAPI);
+
+      // console.log(dataAPI);
+
+    }
+    getRepo();
+
+
+
+
+  }, []);
+
+
+  let handleCountryChange = async (country) => {
+    const dataAPII = await fetchData(country);
+
+    setstate(dataAPII);
+
+
+
+    console.log(dataAPII);
+    //console.log(country);
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+
+
+
+      <img className={styles.image} src={coronaimage} alt="covid" />
+
+      <Cards data={state} />
+
+      <CountryPicker handleCountryChange={handleCountryChange} country={country} />
+      <Chart data={state} country={country} />
+
+
     </div>
   );
 }
